38a8e3494ad28be221915aad308f6966cbfc8b9e
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-02-17 00:54:34 +0800

    completed prims mst

--- algorithms/PrimMST.java
+++ algorithms/PrimMST.java
@@ -4,25 +4,62 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class PrimMST {
-    
+
+    private Graph g;
     private static final int NO_PARENT = -1;
+
     private PriorityQueue<Edge> minHeap;
     private boolean[] visited;
-    private static final int TOTAL = 7;
+    private int n;
+
     public int[] parent;
-    
-    public PrimMST() {
-        this.parent = new int[TOTAL];
-        this.visited = new boolean[TOTAL];   
-        Arrays.fill(parent, NO_PARENT); 
-        this.minHeap = new PriorityQueue<Edge>((a, b) -> Integer.compare(a.getWeight(), b.getWeight()));
+
+    public PrimMST(Graph g) {
+        this.g = g;
+        this.n = g.size();
+        this.parent = new int[n];
+        this.visited = new boolean[n];
+
+        Arrays.fill(parent, NO_PARENT);
+
+        this.minHeap = new PriorityQueue<>((a, b) -> Double.compare(a.getWeight(), b.getWeight()));
     }
-    
+
     public int[] mstCalc(int start) {
-        primMSThelper(start, n);
+        mstCalcHelper(start);
         return this.parent;
     }
-    
-    
-         
-     
+
+    private void mstCalcHelper(int start) {
+        List<Edge> vertexAdjacencyList = this.g.getNeighbors(start);
+        visited[start] = true;
+        
+        for (Edge e : vertexAdjacencyList) {
+            this.minHeap.add(e);
+        }
+        
+        int count = 0;
+        while(count < n - 1 && !minHeap.isEmpty()) {
+            Edge min = minHeap.poll();
+            int destIndex = min.getDest();
+            
+            if (visited[destIndex]) {
+                continue;
+            }
+            
+            count++;
+            
+            visited[destIndex] = true;
+            parent[destIndex] = min.getSrc();
+            
+            List<Edge> vertexAdjacencyList2 = this.g.get(min.getDest());
+                
+            for (Edge e : vertexAdjacencyList2) {
+                if (!visited[e.getDest()]) {
+                    this.minHeap.add(e);
+                }
+            }
+        }
+    }
+}
+
