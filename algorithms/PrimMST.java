7e2fe415c7129f52f5ba20bf12d7015de4d6f4b4
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-02-16 21:33:36 +0800

    working on Prim

--- algorithms/PrimMST.java
+++ algorithms/PrimMST.java
@@ -3,3 +3,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
+public class PrimMST {
+    
+    private static final int NO_PARENT = -1;
+    private PriorityQueue<Edge> minHeap;
+    private boolean[] visited;
+    private static final int TOTAL = 7;
+    public int[] parent;
+    
+    public PrimMST() {
+        this.parent = new int[TOTAL];
+        this.visited = new boolean[TOTAL];   
+        Arrays.fill(parent, NO_PARENT); 
+        this.minHeap = new PriorityQueue<Edge>((a, b) -> Integer.compare(a.getWeight(), b.getWeight()));
+    }
+    
+    public int[] mstCalc(int start) {
+        primMSThelper(start, n);
+        return this.parent;
+    }
+    
+    
+         
+     
