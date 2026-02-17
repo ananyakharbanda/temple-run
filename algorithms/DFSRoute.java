055e266281d3b92806b0ac1fe867ba972e87fa0c
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-02-17 15:18:29 +0800

    convert parent list to adjacency list for undirected graph dfs traversal

--- algorithms/DFSRoute.java
+++ algorithms/DFSRoute.java
@@ -1,4 +1,17 @@
 package algorithms;
+    
+import java.util.List;
 
 public class DFSRoute {
     
+    private List<List<Integer>> mstTree;
+
+    public DFSRoute() {
+        this.mstTree = mstTree;
+    }
+    
+    public void dfs(Edge node) {
+        visited[node] = true;
+        route.add(node);    
+        
+        for (
